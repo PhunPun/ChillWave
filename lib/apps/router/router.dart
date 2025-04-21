ebb6d7001875414dcade18c97703c5b7b0b7536b
Phun <167426498+PhunPun@users.noreply.github.com>
2025-04-21 23:01:54 +0700

    set up project

---
+++ lib/apps/router/router.dart
@@ -0,0 +1,44 @@
+
+import 'package:chillwave/apps/router/router_name.dart';
+import 'package:chillwave/pages/welcome/welcome_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
+
+class RouterCustum{
+  static final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      name: RouterName.welcome,
+      builder: (BuildContext context, GoRouterState state) {
+        return const WelcomePage();
+      },
+      // routes: <RouteBase>[
+      //   GoRoute(
+      //     path: 'login',
+      //     name: RouterName.login,
+      //     builder: (BuildContext context, GoRouterState state) {
+      //       return const LoginPage();
+      //     },
+      //     routes: [
+      //       GoRoute(
+      //         path: 'home',
+      //         name: RouterName.home,
+      //         builder: (BuildContext context, GoRouterState state) {
+      //           return const HomePage();
+      //         },
+      //       ),
+      //     ]
+      //   ),
+      //   GoRoute(
+      //     path: 'register',
+      //     name: RouterName.register,
+      //     builder: (BuildContext context, GoRouterState state) {
+      //       return const RegisterPage();
+      //     },
+      //   ),
+      // ],
+    ),
+  ],
+);
+}
---
+++ lib/themes/colors/colors.dart
@@ -0,0 +1,17 @@
+class MyColor{
+  static const int white = 0xFFFFFFFF;
+  static const int black = 0xFF000000;
+  static const int pr1 = 0xFFFDEFF4;
+  static const int pr2 = 0xFFF4D9D0;
+  static const int pr3 = 0xFFFFC0D3;
+  static const int pr4 = 0xFFFF5C8D;
+  static const int pr5 = 0xFFC75B7A;
+  static const int pr6 = 0xFF921A40;
+  static const int se1 = 0xFFA9B5DF;
+  static const int se2 = 0xFF37B7C3;
+  static const int se3 = 0xFF088395;
+  static const int se4 = 0xFF071952;
+  static const int se5 = 0xFF010A43;
+  static const int grey = 0xFFA19E9A;
+  static const int red = 0xFFF13535;
+}
